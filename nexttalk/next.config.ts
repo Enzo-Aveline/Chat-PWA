8ab79761d025b0c5a997bb77dd4bbd40f341275e
Enzo Tessier <enzo.tessier72@gmail.com>
2025-10-02 13:22:59 +0200

    init PWA

--- nexttalk/next.config.ts
+++ nexttalk/next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+const withPWA = require('next-pwa')({ dest: 'public', disable: process.env.NODE_ENV === 'development' })
+module.exports = withPWA({ /* next config */ })
+
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
